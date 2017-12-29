a09ff8acc329fe994290559237778a5208b2fcc3
psychopath <fanxiaotongA3_love@163.com>
2017-12-29 15:10:47 +0800

    wait

--- src/app/components/manage/managepage/setting/setting.component.ts
+++ src/app/components/manage/managepage/setting/setting.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
--- src/app/components/manage/order/order-result/order-result.component.ts
+++ src/app/components/manage/order/order-result/order-result.component.ts
@@ -11,5 +11,5 @@ export class OrderResultComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
+
